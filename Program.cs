3ba5def5ffa9635ca960b3fb85ae647af5962222
shannon <shannonpetersen33@gmail.com>
2026-01-09 17:26:29 +0200

    Add logic for game history and optimise gameplay rounds

--- Program.cs
+++ Program.cs
@@ -23,17 +23,33 @@
 */
 
 // **Global Variables**
+Random numbers = new Random();
+int leftHandOperand = numbers.Next(1, 101);
+int rightHandOperand = numbers.Next(1,101);
+int answer;
+int newUserAnswer;
 int userScore = 0;
 int maxRounds = 5;
+string? userAnswer;
+string result;
+bool gameRunning = true;
+List<object[]> gameHistory = new List<object[]>();
+
+  
 
 
-Console.WriteLine("Choose an option:\n1) Addition Math Questions\n2) Subtraction Math Questions\n3) Multiplication Math Questions\n4) Division Math Questions\n5) Random Operator Math Questions\n6) Game History\n7) Exit");
+while (gameRunning)
+{
 Menu();
+}
 
 //--Methods--
 
 void Menu()
-{ string? menuSelection = Console.ReadLine();
+{ 
+  Console.WriteLine("Choose an option:\n1) Addition Math Questions\n2) Subtraction Math Questions\n3) Multiplication Math Questions\n4) Division Math Questions\n5) Random Operator Math Questions\n6) Game History\n7) Exit");
+  
+  string? menuSelection = Console.ReadLine();
   
   if (menuSelection != null)
     {
@@ -44,38 +60,161 @@ void Menu()
     case 1:
         Console.WriteLine("Addition Math Game:");
         for(int i = 0; i < maxRounds; i++){
-          PlayRound("addition");
+
+          leftHandOperand = numbers.Next(1, 101);
+          rightHandOperand = numbers.Next(1,101);
+
+          answer = leftHandOperand + rightHandOperand;
+
+          Console.WriteLine( $"{leftHandOperand} + {rightHandOperand} = ??" );
+
+          userAnswer = Console.ReadLine();
+          int.TryParse(userAnswer, out newUserAnswer);
+          if (newUserAnswer == answer)
+          {
+            userScore += 1;
+            Console.WriteLine($"Correct {leftHandOperand} + {rightHandOperand} = {answer}");
+            result = "Win";
+          }
+
+          else 
+          {Console.WriteLine($"Wrong. The Answer is {answer}");
+            result = "Lose";
+          }
+          
+
+          gameHistory.Add(new object[] { 
+            $"{leftHandOperand} + {rightHandOperand}", 
+            answer, 
+            result });
         }
+
         Console.WriteLine($"Game Over\nYour Final score is: {userScore}/5");
+         ReturnToMenu();
         userScore = 0;
         break;
 
     case 2:
         Console.WriteLine("Subtraction Math Game:");
         for(int i = 0; i < maxRounds; i++){
-          PlayRound("subtraction");
-        }
-        Console.WriteLine($"Game Over\nYour Final score is: {userScore}/5");
-        userScore = 0;
+
+          leftHandOperand = numbers.Next(1, 101);
+          rightHandOperand = numbers.Next(1,101);
+
+          if (rightHandOperand > leftHandOperand)
+          {
+            (leftHandOperand, rightHandOperand) = (rightHandOperand, leftHandOperand);
+          }
+          answer = leftHandOperand - rightHandOperand;
+          Console.WriteLine( $"{leftHandOperand} - {rightHandOperand} = ??" );
+
+          userAnswer = Console.ReadLine();
+          int.TryParse(userAnswer, out newUserAnswer);
+
+          if (newUserAnswer == answer)
+          {
+            userScore += 1;
+            Console.WriteLine($"Correct {leftHandOperand} - {rightHandOperand} = {answer}");
+            result = "Win";
+          }
+
+          else 
+          {
+            Console.WriteLine($"Wrong. The Answer is {answer}");
+            result = "Lose";
+          }
+
+         
+          gameHistory.Add(new object[] { 
+            $"{leftHandOperand} - {rightHandOperand}", 
+            answer, 
+            result });
+          }
+            Console.WriteLine($"Game Over\nYour Final score is: {userScore}/5");
+            userScore = 0;
+            ReturnToMenu();
+       
         break;
 
     case 3:
-        for(int i = 0; i < maxRounds; i++){
-          PlayRound("multiplication");
+       
+        for(int i = 0; i < maxRounds; i++)
+        {
+          int multiplyLeftHandOperands = numbers.Next(1, 101);
+          int multiplyRightHandOperands = numbers.Next(1, 101);
+          answer = multiplyLeftHandOperands * multiplyRightHandOperands;
+          Console.WriteLine( $"{multiplyLeftHandOperands} * {multiplyRightHandOperands} = ??" );
+          userAnswer = Console.ReadLine();
+          int.TryParse(userAnswer, out newUserAnswer);
+          if (newUserAnswer == answer)
+          {
+            userScore += 1;
+            Console.WriteLine($"Correct {multiplyLeftHandOperands} * {multiplyRightHandOperands} = {answer}");
+            result = "Win";
+          }
+
+          else {
+            Console.WriteLine($"Wrong. The Answer is {answer}");
+            result = "Lose";
+          }
+          gameHistory.Add(new object[] { 
+            $"{multiplyLeftHandOperands} * {multiplyRightHandOperands}", 
+            answer, 
+            result });
+          
+         
         }
+        
+    
         Console.WriteLine($"Game Over\nYour Final score is: {userScore}/5");
         userScore = 0;
         break;
 
     case 4:
-        for(int i = 0; i < maxRounds; i++){
-          PlayRound("division");
+      
+        for(int i = 0; i < maxRounds; i++)
+      {
+        int dividend;
+        int divisor; 
+    
+    
+        do{
+          dividend = numbers.Next(1, 101);
+          divisor = numbers.Next(1, 101);
+    
+        } while(dividend % divisor != 0 || dividend < divisor);
+    
+        answer = dividend / divisor;
+    
+        Console.WriteLine( $"{dividend} / {divisor} = ??" );
+        userAnswer = Console.ReadLine();
+        int.TryParse(userAnswer, out newUserAnswer);
+        if (newUserAnswer == answer)
+        {
+          userScore += 1;
+          Console.WriteLine($"Correct {dividend} / {divisor} = {answer}");
+          result = "Win";
         }
+
+        else {
+          Console.WriteLine($"Wrong. The Answer is {answer}");
+          result = "Lose";
+        }
+          
+        gameHistory.Add(new object[] { 
+            $"{dividend} / {divisor}", 
+            answer, 
+            result });
+        
+        }
+          
+
         Console.WriteLine($"Game Over\nYour Final score is: {userScore}/5");
         userScore = 0;
         break;
 
     case 5:
+      Console.Clear();
         Random option = new Random();
        Console.WriteLine("Random Operator Math Game:");
         for(int i = 0; i < maxRounds; i++){
@@ -102,116 +241,80 @@ void Menu()
         break;
 
     case 6:
-        Console.WriteLine("Option 6 will be here - Game History");
+        GameRecords();
+        ReturnToMenu();
         break;
 
     case 7:
+        gameRunning = false;
         Console.WriteLine("Goodbye :)");
+        
         break;
 
     default:
         Console.WriteLine("Enter a number option");
-        Menu();
+        ReturnToMenu();
         break;
     
     
   }
     }
 }
+void ReturnToMenu()
+{ 
+  Console.WriteLine("Press Enter to return to the Main Menu.");
+  Console.ReadLine();
+  Console.Clear();
+  Console.Write("\x1b[3J");
+  Console.Clear();
+  Menu();
+}
+
+
+
+
+
 void PlayRound(string GameOption)
 {
-  Random numbers = new Random();
-  int leftHandOperand = numbers.Next(1, 101);
-  int rightHandOperand = numbers.Next(1,101);
-  int answer;
-  string? userAnswer;
-  int newUserAnswer;
+
 
   switch(GameOption){
 
     case "addition" :
-      answer = leftHandOperand + rightHandOperand;
-      Console.WriteLine( $"{leftHandOperand} + {rightHandOperand} = ??" );
-      userAnswer = Console.ReadLine();
-      int.TryParse(userAnswer, out newUserAnswer);
-      if (newUserAnswer == answer)
-      {
-        userScore += 1;
-        Console.WriteLine($"Correct {leftHandOperand} + {rightHandOperand} = {answer}");
-      }
-
-      else Console.WriteLine($"Wrong. The Answer is {answer}");
+      
 
     break;
 
     case "subtraction" :
-    if (rightHandOperand > leftHandOperand)
-    {
-      (leftHandOperand, rightHandOperand) = (rightHandOperand, leftHandOperand);
-    }
-      answer = leftHandOperand - rightHandOperand;
-      Console.WriteLine( $"{leftHandOperand} - {rightHandOperand} = ??" );
-      userAnswer = Console.ReadLine();
-      int.TryParse(userAnswer, out newUserAnswer);
-      if (newUserAnswer == answer)
-      {
-        userScore += 1;
-        Console.WriteLine($"Correct {leftHandOperand} - {rightHandOperand} = {answer}");
-      }
-
-      else Console.WriteLine($"Wrong. The Answer is {answer}");
+    
 
 
     break;
 
     case "multiplication" :
-      int multiplyLeftHandOperands = numbers.Next(1, 101);
-      int multiplyRightHandOperands = numbers.Next(1, 101);
-      answer = multiplyLeftHandOperands * multiplyRightHandOperands;
-      Console.WriteLine( $"{multiplyLeftHandOperands} * {multiplyRightHandOperands} = ??" );
-      userAnswer = Console.ReadLine();
-      int.TryParse(userAnswer, out newUserAnswer);
-      if (newUserAnswer == answer)
-      {
-        userScore += 1;
-        Console.WriteLine($"Correct {multiplyLeftHandOperands} * {multiplyRightHandOperands} = {answer}");
-      }
-
-      else Console.WriteLine($"Wrong. The Answer is {answer}");
+      
 
     break;
 
     case "division" :
-    int dividend;
-    int divisor; 
     
-    
-    do{
-    dividend = numbers.Next(1, 101);
-    divisor = numbers.Next(1, 101);
-    
-    } while(dividend % divisor != 0 || dividend < divisor);
-    
-     answer = dividend / divisor;
-    
-      Console.WriteLine( $"{dividend} / {divisor} = ??" );
-      userAnswer = Console.ReadLine();
-      int.TryParse(userAnswer, out newUserAnswer);
-      if (newUserAnswer == answer)
-      {
-        userScore += 1;
-        Console.WriteLine($"Correct {dividend} / {divisor} = {answer}");
-      }
-
-      else Console.WriteLine($"Wrong. The Answer is {answer}");
 
 
     break;
   }
 }
 
-//void GameRecord(){}
+void GameRecords()
+{ 
+  Console.WriteLine("\n\tHistory Table:\n");
+  Console.WriteLine("Problem\t\tAnswer\t\tResult");
+  foreach (object[] game in gameHistory)
+{
+    Console.WriteLine($"{game[0]}\t\t{game[1]}\t\t{game[2]}");
+}
+Console.WriteLine();
 
-// void PlayGame(){}
+}
+
 
 
